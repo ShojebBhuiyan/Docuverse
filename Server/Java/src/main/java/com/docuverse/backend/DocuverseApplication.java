a803958f61b8122f97b9044ef084a9f9926c0bd5
ShojebBhuiyan <shojeb.bhuiyan@gmail.com>
2023-08-31 22:46:55 +0600

    Spring server init

---
+++ Server/Java/src/main/java/com/docuverse/backend/DocuverseApplication.java
@@ -0,0 +1,13 @@
+package com.docuverse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocuverseApplication.class, args);
+	}
+
+}
---
+++ Server/Java/src/test/java/com/docuverse/backend/DocuverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docuverse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
