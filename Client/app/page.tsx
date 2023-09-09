d3c88b655401d1d0221fa85fc375df22a5234fda
farabi <rafid.nahiyan.farabi.85@gmail.com>
2023-09-09 11:58:34 +0600

    Added card in home page

--- Client/app/page.tsx
+++ Client/app/page.tsx
@@ -1,7 +1,20 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import { Card,CardTitle } from "@/components/ui/card"
+import Link  from 'next/link'
+
 export default function IndexPage() {
+  
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      
-    </section>
+    <div className="flex py-40 justify-center gap-8">
+       <Card className="w-48 h-32 flex items-center justify-center ">
+       <CardTitle>Ask</CardTitle>
+       </Card>
+       <Card className="w-48 h-32 flex items-center justify-center ">
+       <CardTitle>
+        <Link href="/"> Answer</Link>
+       </CardTitle>
+       </Card>
+    </div>
   )
 }
