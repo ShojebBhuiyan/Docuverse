27af3add7f0ce11f7123b29e4e76d619e0c8d8c7
farabi <rafid.nahiyan.farabi.85@gmail.com>
2023-09-10 20:06:46 +0600

    added flashcard flip

--- Client/app/decks/[id]/page.tsx
+++ Client/app/decks/[id]/page.tsx
@@ -10,6 +10,7 @@ const page =  () => {
     const { id } = useParams()
     const [count, setCount] = useState(0);
     const [data, setData] = useState<PostProps[]>();
+    const [front, setFront] = useState(true);
 
     useEffect(() => {
       fetch('https://jsonplaceholder.typicode.com/posts')
@@ -29,14 +30,23 @@ const page =  () => {
 
     const handleClickRight = (e: React.MouseEvent<HTMLButtonElement>) => {
       //e.preventDefault();
+      setFront(true);
       setCount(count + 1);
+    };
+
+    const handleCard = (e: React.MouseEvent<HTMLDivElement>) => {
+      //e.preventDefault();
+      setFront(true);
+      setFront(!front);
     };
     
   return (
     <div>
-    <div className='flex p-5 border ml-10 mt-20 w-[800px] h-[400px] justify-center items-center '>  
-        <h1>Deck {id} : {count}  </h1>
-        {data && <h1>{data[count].title}</h1>}
+    <div className='flex flex-col p-5 border ml-10 mt-20 w-[800px] h-[400px] justify-center items-center' onClick={handleCard}>  
+        <h1>Deck {id} : {count}</h1>
+        <h1>{front ? 'Front' : 'Back'}</h1>
+        <h2>{front ? data?.[count]?.title : data?.[count]?.body}</h2>
+        
     </div>
     <div className="flex ml-10 mt-2 w-[800px] justify-center gap-4 ">
     <Button className="" onClick={handleClickLeft} variant="outline" size="icon">
