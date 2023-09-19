8ef1559457ae89071b5f3a6f90580308e5d81324
ShojebBhuiyan <shojeb.bhuiyan@gmail.com>
2023-09-19 23:28:27 +0600

    Auth Manager

--- Server/Java/src/main/java/com/docuverse/backend/configuration/SecurityConfiguration.java
+++ Server/Java/src/main/java/com/docuverse/backend/configuration/SecurityConfiguration.java
@@ -2,7 +2,11 @@ package com.docuverse.backend.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +19,12 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider daoProvider = new DaoAuthenticationProvider();
+        daoProvider.setUserDetailsService(userDetailsService);
+        return new ProviderManager(daoProvider);
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
