a92577aedc3a0ac9210d2f90e44ca920c31f0515
ShojebBhuiyan <shojeb.bhuiyan@gmail.com>
2023-09-19 22:50:23 +0600

    Removed subscription model, added subscription enum, modified user model

--- Server/Java/src/main/java/com/docuverse/backend/model/User.java
+++ Server/Java/src/main/java/com/docuverse/backend/model/User.java
@@ -1,10 +1,15 @@
 package com.docuverse.backend.model;
 
+import com.docuverse.backend.enums.Subscription;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Table(name = "User")
-public class User {
+public class User implements UserDetails, GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -15,14 +20,19 @@ public class User {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "subscription")
+    private Subscription subscription;
+
+
     public User() {
 
     }
 
-    public User(String name, String email, String password) {
+    public User(String name, String email, String password, Subscription subscription) {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.subscription = subscription;
 //        this.thread = thread;
     }
 
@@ -50,12 +60,59 @@ public class User {
         this.email = email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
     public String getPassword() {
         return password;
     }
 
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.subscription.toString();
+    }
+
+    public void setAuthority(Subscription authority) {
+        this.subscription = authority;
+    }
+
+    public String getSubscription() {
+        return this.subscription.toString();
+    }
+
+    public void setSubscription(Subscription subscription) {
+        this.subscription = subscription;
     }
 }
 
