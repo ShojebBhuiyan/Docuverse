cf6297e24034f738a589d6310a9e514984bfa419
ShojebBhuiyan <shojeb.bhuiyan@gmail.com>
2023-09-20 00:20:15 +0600

    SignupDTO and User Signup API

---
+++ Server/Java/src/main/java/com/docuverse/backend/controllers/AuthController.java
@@ -0,0 +1,22 @@
+package com.docuverse.backend.controllers;
+
+import com.docuverse.backend.models.SignUpDTO;
+import com.docuverse.backend.models.User;
+import com.docuverse.backend.services.AuthenticationServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AuthController {
+    @Autowired
+    private AuthenticationServices authenticationServices;
+
+    @PostMapping("/signup")
+    public User signUpUser(@RequestBody SignUpDTO body) {
+        return authenticationServices.signUpUser(
+                body.getUsername(), body.getEmail(), body.getPassword()
+        );
+    }
+}
