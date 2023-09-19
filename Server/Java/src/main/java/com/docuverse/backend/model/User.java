9a9ca64f274833302c7d0d4a8eb799af981ab7b3
ShojebBhuiyan <shojeb.bhuiyan@gmail.com>
2023-09-19 22:23:11 +0600

    Spring Security config init

--- Server/Java/src/main/java/com/docuverse/backend/model/User.java
+++ Server/Java/src/main/java/com/docuverse/backend/model/User.java
@@ -14,9 +14,6 @@ public class User {
     private String email;
     @Column(name = "password")
     private String password;
-//    @ManyToOne
-//    @JoinColumn(name = "threads")
-//    private Thread thread;
 
     public User() {
 
@@ -61,46 +58,5 @@ public class User {
         this.password = password;
     }
 }
-
-//package com.docuverse.backend.model;
-//
-//        import jakarta.persistence.*;
-//
-//@Entity
-//@Table(name = "Thread")
-//public class Thread {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private long id;
-//    @Column(name = "name")
-//    private long name;
-//    @Column(name = "vectorIndex")
-//    private long vectorIndex;
-//    @Column(name = "chatLog")
-//    private String chatLog;
-//    @ManyToOne
-//    @JoinColumn(name = "files")
-//    private File file;
-//
-//}
-
-
-//package com.docuverse.backend.model;
-//
-//        import jakarta.persistence.*;
-//
-//@Entity
-//@Table(name = "file")
-//public class File {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private long id;
-//    @Column(name = "name")
-//    private String name;
-//    @Column(name = "url")
-//    private String url;
-//    @Column(name = "textContent")
-//    private String textContent;
-//}
 
 
