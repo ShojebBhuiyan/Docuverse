c98126dc7175adbff2e9da5b741d88163d39081f
farabi <rafid.nahiyan.farabi.85@gmail.com>
2023-09-21 06:32:01 +0600

    Langchain init

---
+++ Server/Java/src/main/java/com/docuverse/backend/configuration/PineconeConfiguration.java
@@ -0,0 +1,42 @@
+package com.docuverse.backend.configuration;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.openai.OpenAiEmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.pinecone.PineconeEmbeddingStore;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import java.net.URL;
+
+import static dev.langchain4j.model.openai.OpenAiModelName.TEXT_EMBEDDING_ADA_002;
+import static dev.langchain4j.data.document.UrlDocumentLoader.load;
+
+public class PineconeConfiguration {
+    Dotenv dotenv = Dotenv.load();
+
+    @Bean
+    EmbeddingStore<TextSegment> embeddingStore(EmbeddingModel embeddingModel, ResourceLoader resourceLoader) {
+        EmbeddingStore<TextSegment> embeddingStore = PineconeEmbeddingStore
+                .builder()
+                .apiKey(dotenv.get("PINECONE_API_KEY"))
+                .environment(dotenv.get("PINECONE_ENVIRONMENT"))
+                .projectId(dotenv.get("PINECONE_PROJECTID"))
+                .index(dotenv.get("PINECONE_INDEX"))
+                .build();
+
+//        URL fileUrl = "some";
+//        Document document = load()
+        return embeddingStore;
+    }
+
+
+    OpenAiEmbeddingModel embeddingModel = OpenAiEmbeddingModel.builder()
+            .apiKey(dotenv.get("OPENAI_API_KEY"))
+            .modelName(TEXT_EMBEDDING_ADA_002)
+            .build();
+}
