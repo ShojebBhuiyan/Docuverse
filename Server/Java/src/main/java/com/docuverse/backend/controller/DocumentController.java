008ab98cba9512c06bc99e06f0787fd089d0dfa0
ShojebBhuiyan <shojeb.bhuiyan@gmail.com>
2023-09-25 13:44:13 +0600

    File upload backend service_2

--- Server/Java/src/main/java/com/docuverse/backend/controller/DocumentController.java
+++ Server/Java/src/main/java/com/docuverse/backend/controller/DocumentController.java
@@ -1,2 +1,41 @@
-package com.docuverse.backend.controller;public class DocumentController {
+package com.docuverse.backend.controller;
+
+import com.docuverse.backend.services.DocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@RestController
+@RequestMapping("/api/v1/document")
+public class DocumentController {
+    @Autowired
+    DocumentService documentService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadPDFs(@RequestParam("pdfFiles") MultipartFile[] pdfFiles) {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+        for (MultipartFile pdfFile : pdfFiles) {
+            executorService.submit(() -> {
+                try {
+                    String text = documentService.extractTextFromPDF(pdfFile);
+                    // Process or store the extracted text as needed.
+                } catch (IOException e) {
+                    // Handle exceptions appropriately.
+                }
+            });
+        }
+
+        executorService.shutdown();
+
+        return ResponseEntity.ok("Upload successful");
+    }
 }
--- Server/Java/src/main/java/com/docuverse/backend/services/DocumentService.java
+++ Server/Java/src/main/java/com/docuverse/backend/services/DocumentService.java
@@ -1,2 +1,29 @@
-package com.docuverse.backend.services;public class DocumentService {
+package com.docuverse.backend.services;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class DocumentService {
+    public String extractTextFromPDF(MultipartFile pdfFile) throws IOException {
+        try (InputStream is = pdfFile.getInputStream();
+             PDDocument document = Loader.loadPDF(is.readAllBytes());) {
+
+            PDFTextStripper stripper = new PDFTextStripper();
+            String text = stripper.getText(document);
+
+            System.out.println(text);
+            document.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
