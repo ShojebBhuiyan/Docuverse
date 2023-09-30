89aa341b1b994bb834084ad58a4fcbe197229a37
Md. Shahriar Islam Bhuiyan <shojeb.bhuiyan@gmail.com>
2023-10-01 05:43:32 +0600

    Navbar

--- Client/config/site.ts
+++ Client/config/site.ts
@@ -1,13 +1,13 @@
-export type SiteConfig = typeof siteConfig
+export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Docuverse",
-  description:
-    "Talk to your documents!",
+  description: "Talk to your documents!",
   mainNav: [
-
+    {
+      title: "Features",
+      href: "/features",
+    },
   ],
-  links: {
-
-  },
-}
+  links: {},
+};
